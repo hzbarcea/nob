245a16961457f31fa80dea05b9b8db0d58c9a95e
Ciprian Ciubotariu <cheepeero@gmx.net>
2015-01-31 16:49:34 +0200

    Whitespace: replace tabs with spaces

--- nob.go
+++ nob.go
@@ -1,128 +1,128 @@
 package main
 
 import (
-	"github.com/codegangsta/cli"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"log"
-	"os"
+    "github.com/codegangsta/cli"
+    "gopkg.in/yaml.v2"
+    "io/ioutil"
+    "log"
+    "os"
 )
 
 func createNobService(c *cli.Context) (NobService, error) {
-	configFile := c.GlobalString("config")
-	contents, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		log.Fatalln("unable to read config file:", err)
-		return NobService{}, err
-	}
+    configFile := c.GlobalString("config")
+    contents, err := ioutil.ReadFile(configFile)
+    if err != nil {
+        log.Fatalln("unable to read config file:", err)
+        return NobService{}, err
+    }
 
-	var config map[string]NobService
-	config = make(map[string]NobService)
-	err = yaml.Unmarshal(contents, config)
-	if err != nil {
-		log.Fatalln("cannot parse ", configFile, ":", err)
-		return NobService{}, err
-	}
+    var config map[string]NobService
+    config = make(map[string]NobService)
+    err = yaml.Unmarshal(contents, config)
+    if err != nil {
+        log.Fatalln("cannot parse ", configFile, ":", err)
+        return NobService{}, err
+    }
 
-	nob := c.GlobalString("nob")
-	service := config[nob]
-	if service.Url == "" {
-		log.Fatalln("no nob service '"+nob+"' defined in file ", configFile)
-		return NobService{}, err
-	}
+    nob := c.GlobalString("nob")
+    service := config[nob]
+    if service.Url == "" {
+        log.Fatalln("no nob service '"+nob+"' defined in file ", configFile)
+        return NobService{}, err
+    }
 
-	return service, nil
+    return service, nil
 }
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "nob"
-	app.Usage = "ActiveMQ network of brokers manager"
-	app.Version = "1.0"
-	app.Author = "Hadrian Zbarcea"
-	app.Email = "hadrian@apache.org"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "config, f",
-			Value: os.Getenv("HOME") + "/.nobrc",
-			Usage: "nob client configuration file",
-		},
-		cli.StringFlag{
-			Name:  "nob, n",
-			Value: "",
-			Usage: "nob service definition from the config file",
-		},
-	}
-	app.Commands = []cli.Command{
-		{
-			Name:        "broker",
-			ShortName:   "b",
-			Usage:       "usage of the broker command",
-			Description: "Describe the usage of the broker command",
-			Subcommands: []cli.Command{
-				{
-					Name:        "list",
-					ShortName:   "l",
-					Usage:       "print a list of brokers",
-					Description: "Description of how the list of brokers is printed",
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:  "filter",
-							Value: "",
-							Usage: "Filter the brokers to print",
-						},
-					},
-					Action: func(c *cli.Context) {
-						service, err := createNobService(c)
-						if err != nil {
-							println("could not fetch broker list:", err)
-							return
-						}
-						ListBrokers(service, c.String("filter"))
-					},
-				}, {
-					Name:        "create",
-					ShortName:   "c",
-					Usage:       "create a new broker",
-					Description: "Description of how to create a new broker",
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name: "name",
-							//							Value: "",
-							Usage: "Name of the new broker",
-						},
-					},
-					Action: func(c *cli.Context) {
-						service, err := createNobService(c)
-						if err != nil {
-							println("could not create broker:", err)
-							return
-						}
-						CreateBroker(service, c.String("name"))
-					},
-				}, {
-					Name:      "info",
-					ShortName: "i",
-					Usage:     "prints broker metadata",
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:  "id",
-							Value: "",
-							Usage: "id of the broker",
-						},
-					},
-					Action: func(c *cli.Context) {
-						service, err := createNobService(c)
-						if err != nil {
-							println("could not get broker info:", err)
-							return
-						}
-						BrokerInfo(service, c.String("id"))
-					},
-				},
-			},
-		},
-	}
+    app := cli.NewApp()
+    app.Name = "nob"
+    app.Usage = "ActiveMQ network of brokers manager"
+    app.Version = "1.0"
+    app.Author = "Hadrian Zbarcea"
+    app.Email = "hadrian@apache.org"
+    app.Flags = []cli.Flag{
+        cli.StringFlag{
+            Name:  "config, f",
+            Value: os.Getenv("HOME")+"/.nobrc",
+            Usage: "nob client configuration file",
+        },
+        cli.StringFlag{
+            Name:  "nob, n",
+            Value: "",
+            Usage: "nob service definition from the config file",
+        },
+    }
+    app.Commands = []cli.Command{
+        {
+            Name:        "broker",
+            ShortName:   "b",
+            Usage:       "usage of the broker command",
+            Description: "Describe the usage of the broker command",
+            Subcommands: []cli.Command{
+                {
+                    Name:        "list",
+                    ShortName:   "l",
+                    Usage:       "print a list of brokers",
+                    Description: "Description of how the list of brokers is printed",
+                    Flags: []cli.Flag{
+                        cli.StringFlag{
+                            Name:  "filter",
+                            Value: "",
+                            Usage: "Filter the brokers to print",
+                        },
+                    },
+                    Action: func(c *cli.Context) {
+                        service, err := createNobService(c)
+                        if err != nil {
+                            println("could not fetch broker list:", err)
+                            return
+                        }
+                        ListBrokers(service, c.String("filter"))
+                    },
+                }, {
+                    Name:        "create",
+                    ShortName:   "c",
+                    Usage:       "create a new broker",
+                    Description: "Description of how to create a new broker",
+                    Flags: []cli.Flag{
+                        cli.StringFlag{
+                            Name: "name",
+                            //							Value: "",
+                            Usage: "Name of the new broker",
+                        },
+                    },
+                    Action: func(c *cli.Context) {
+                        service, err := createNobService(c)
+                        if err != nil {
+                            println("could not create broker:", err)
+                            return
+                        }
+                        CreateBroker(service, c.String("name"))
+                    },
+                }, {
+                    Name:      "info",
+                    ShortName: "i",
+                    Usage:     "prints broker metadata",
+                    Flags: []cli.Flag{
+                        cli.StringFlag{
+                            Name:  "id",
+                            Value: "",
+                            Usage: "id of the broker",
+                        },
+                    },
+                    Action: func(c *cli.Context) {
+                        service, err := createNobService(c)
+                        if err != nil {
+                            println("could not get broker info:", err)
+                            return
+                        }
+                        BrokerInfo(service, c.String("id"))
+                    },
+                },
+            },
+        },
+    }
 
-	app.Run(os.Args)
+    app.Run(os.Args)
 }
--- nobservice.go
+++ nobservice.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
 )
 
 type NobService struct {
-	Url                string
-	Username, Password string
+    Url                string
+    Username, Password string
 }
 
 func call(service NobService, method string, url string, reqBody io.Reader) string {
-	fmt.Println("request:", method, url)
+    fmt.Println("request:", method, url)
 
-	req, err := http.NewRequest("GET", url, reqBody)
-	if err != nil {
-		panic(err)
-	}
+    req, err := http.NewRequest("GET", url, reqBody)
+    if err != nil {
+        panic(err)
+    }
 
-	if service.Username != "" {
-		fmt.Println("request: auth HTTP Basic as: ", service.Username)
-		req.SetBasicAuth(service.Username, service.Password)
-	}
-	if reqBody != nil {
-		req.Header.Set("Content-Type", "application/json")
-	}
-	req.Header.Set("Accept", "application/json")
+    if service.Username != "" {
+        fmt.Println("request: auth HTTP Basic as: ", service.Username)
+        req.SetBasicAuth(service.Username, service.Password)
+    }
+    if reqBody != nil {
+        req.Header.Set("Content-Type", "application/json")
+    }
+    req.Header.Set("Accept", "application/json")
 
-	fmt.Println("request: Headers:", req.Header)
+    fmt.Println("request: Headers:", req.Header)
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
 
-	fmt.Println("response: Status:", resp.Status)
-	fmt.Println("response: Headers:", resp.Header)
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("response: Body:", string(respBody))
+    fmt.Println("response: Status:", resp.Status)
+    fmt.Println("response: Headers:", resp.Header)
+    respBody, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("response: Body:", string(respBody))
 
-	return string(respBody)
+    return string(respBody)
 }
 
 func ListBrokers(service NobService, filter string) string {
-	url := service.Url + "/brokers"
-	if filter != "" {
-		url += "?filter="
-		url += filter // hopefully the http object will encode it
-	}
+    url := service.Url + "/brokers"
+    if filter != "" {
+        url += "?filter="
+        url += filter // hopefully the http object will encode it
+    }
 
-	return call(service, "GET", url, nil)
+    return call(service, "GET", url, nil)
 }
 
 func CreateBroker(service NobService, name string) string {
-	url := service.Url + "/brokers?create=" + name
+    url := service.Url + "/brokers?create=" + name
 
-	return call(service, "POST", url, nil)
+    return call(service, "POST", url, nil)
 }
 
 func BrokerInfo(service NobService, id string) string {
-	url := service.Url + "/broker/" + id
+    url := service.Url + "/broker/" + id
 
-	return call(service, "GET", url, nil)
+    return call(service, "GET", url, nil)
 }
