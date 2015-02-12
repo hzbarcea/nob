d6f362e7ed4a3cb7590bea6d14aa5d041b70a6d5
Ciprian Ciubotariu <cheepeero@gmx.net>
2015-02-12 18:13:23 +0200

    Move REST-related functionality into rest.go

--- nobservice.go
+++ nobservice.go
@@ -2,21 +2,11 @@ package main
 
 import (
     "encoding/json"
-    "io"
-    "io/ioutil"
-    "net/http"
     "time"
 )
 
 type NobService struct {
-    Url                string
-    Username, Password string
-}
-
-var noHeaders = http.Header{ }
-var jsonCall = http.Header{
-    "Content-Type": { "application/json" },
-    "Accept": { "application/json"},
+    RestService
 }
 
 // Broker resource representation in activemq-nob-api
@@ -27,7 +17,7 @@ type Broker struct {
     LastModifiedXbean time.Time
 }
 
-// used in parsing weirdly embedded json responses :D
+// used in parsing weirdly embedded json responses
 type brokerList struct {
     BrokerList []Broker `json:"broker"`
 }
@@ -38,45 +28,7 @@ type brokerInfoResponse struct {
     Broker Broker
 }
 
-
-func (service NobService) call(method string, url string, reqBody io.Reader, httpHeaders http.Header) string {
-    TraceLog.Println("request:", method, url)
-
-    req, err := http.NewRequest(method, url, reqBody)
-    if err != nil {
-        panic(err)
-    }
-
-    if service.Username != "" {
-        TraceLog.Println("request: auth HTTP Basic as: ", service.Username)
-        req.SetBasicAuth(service.Username, service.Password)
-    }
-
-    for header, values := range httpHeaders {
-        for it := range values {
-            req.Header.Set(header, values[it])
-        }
-    }
-
-    TraceLog.Println("request: Headers:", req.Header)
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-
-    TraceLog.Println("response: Status:", resp.Status)
-    TraceLog.Println("response: Headers:", resp.Header)
-    respBody, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
-    TraceLog.Println("response: Body:", string(respBody))
-
-    return string(respBody)
-}
+//// NoB service API
 
 func (service NobService) ListBrokers(filter string) []Broker {
     url := service.Url + "/brokers"
@@ -85,7 +37,7 @@ func (service NobService) ListBrokers(filter string) []Broker {
         url += filter // hopefully the http object will encode it
     }
 
-    jsonString := service.call("GET", url, nil, jsonCall)
+    jsonString := service.Call("GET", url, nil, JsonCall)
 
     var brokerList listBrokersResponse
     err := json.Unmarshal([]byte(jsonString), &brokerList)
@@ -94,20 +46,19 @@ func (service NobService) ListBrokers(filter string) []Broker {
     }
 
     return brokerList.Brokers.BrokerList
-
 }
 
 func (service NobService) CreateBroker() string {
     url := service.Url + "/brokers?create"
 
-    brokerId := service.call("POST", url, nil, noHeaders)
+    brokerId := service.Call("POST", url, nil, NoHeaders)
     return brokerId
 }
 
 func (service NobService) BrokerInfo(id string) Broker {
     url := service.Url + "/broker/" + id
 
-    jsonString := service.call("GET", url, nil, jsonCall)
+    jsonString := service.Call("GET", url, nil, JsonCall)
 
     var brokerInfo brokerInfoResponse
     err := json.Unmarshal([]byte(jsonString), &brokerInfo)
